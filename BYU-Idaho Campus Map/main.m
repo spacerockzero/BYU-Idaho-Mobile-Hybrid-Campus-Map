7ed3ca8093aebc4ea19aa1906d9faa044de1163e
Jakob Anderson <ficitonisfiction@gmail.com>
2012-07-25 11:13:56 -0600

    Initial Commit

---
+++ BYU-Idaho Campus Map/main.m
@@ -0,0 +1,5 @@
+//Do NOT remove or move this comment.  It is needed for android app builds.  AndroidSDK version 7
+#import <UIKit/UIKit.h>
+#import "QuickConnectViewController.h"
+        
+        
